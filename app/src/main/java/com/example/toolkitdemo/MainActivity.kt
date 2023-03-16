f8f06dc9a0642817dec60cf581a66af8e01d5b15
smuzo <smuzo41212@gmail.com>
2023-03-16 12:02:57 +0500

    Muzaffar

--- app/src/main/java/com/example/toolkitdemo/MainActivity.kt
+++ app/src/main/java/com/example/toolkitdemo/MainActivity.kt
@@ -2,22 +2,52 @@ package com.example.toolkitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.example.toolkitdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.hadithFragment,
+                R.id.ibadatFragment,
+                R.id.moreFragment,
+                R.id.quranFragment
+            ),binding.drawerLayout
+        )
 
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
         binding.bottomNavView.setupWithNavController(navController)
+        binding.drawerNavigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
 
---
+++ app/src/main/java/com/example/toolkitdemo/read_quran/BySurrahModel.kt
@@ -0,0 +1,3 @@
+package com.example.toolkitdemo.read_quran
+
+class BySurrahModel(val id:Int, val nameSurrahAr: String, val nameSurrahEn:String, val totalVerse:Int)
